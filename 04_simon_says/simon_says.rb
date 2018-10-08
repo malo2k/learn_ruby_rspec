7da870cc8ef55ec25d715b144d03eae2c4ad7b82
Malo De Kervasdoué <43620886+malo2k@users.noreply.github.com>
2018-10-09 00:53:38 +0200

    Add files via upload
    simon_says bcp trop de choses

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
 #write your code here
+def echo(hello)
+	hello
+end
+
+def shout(hello)
+	hello.upcase
+end
+
+def repeat(hello)
+    "#{hello}" " " "#{hello}"
+end
+
+# note au correcteur : à partir de là c'est du grand n'importe quoi, je m'endormis sur mon clvzfvbbbbbbbbbbbbc
+
+# def repeat(hello, n)
+# 	n.times print "#{hello}"
+# end
+
+def start_of_word(hello, n)
+	hello.each{ |n| }
+end
+
+def first_word(hello, n)
+	n = 100
+	hello*n
+end
+
+# gute nacht mein liebe
