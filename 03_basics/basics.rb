df6c61dfc330b27cdd282a769f2651bb1b6746a1
Malo De Kervasdoué <43620886+malo2k@users.noreply.github.com>
2018-10-09 00:56:43 +0200

    Add files via upload
    add des fichiers de l'exercice 3 "basics"

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,34 @@
 # write your code here
+def who_is_bigger(a, b, c)
+	if a == nil || b == nil || c == nil
+	return "nil detected"
+elsif 
+	a > b && a > c
+	return "a is bigger"
+elsif 
+	b > c && b > a
+	return "b is bigger"
+elsif 
+	c > a && c > b
+	return "c is bigger"
+end
+end
+
+def reverse_upcase_noLTA(string)
+	return string.reverse.upcase.delete "LTA"
+end
+
+def array_42(array)
+	array.each do |num|
+		for array in (1..array.size)
+			if num == 42
+			return true
+			end
+		end
+	end
+	return false
+end
+
+def magic_array(array)
+	array.flatten.sort.each{|num| num*2}.delete_if{|num| num %3 == 0}.uniq
+end
