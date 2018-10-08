c28e45945755670cdbde91260a428bb19f4651cd
JessicaBibi <jessica.languille@gmail.com>
2018-10-08 17:37:18 +0200

    ajout calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add(x, y)
+	x + y
+end
+
+def subtract(x, y)
+	x - y 
+end
+
+def sum(array)
+	num = 0 # définit la valeur de départ 
+	array.each do |element| # element prend chaque elements de l'array
+		num = num + element # valeur de départ
+	end
+	return num # on retourne le resultat une fois la boucle finie
+end
+
+def multiply(x, y)
+	x * y
+end
+
+def power(x, y)
+	x ** y
+end
+
+def factorial(n)
+	(1..n).inject(:*)
+end
